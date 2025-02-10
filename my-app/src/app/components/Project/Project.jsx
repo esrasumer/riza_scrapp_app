a2f4dc7a5041a758d54e89e08ec5ba680e66bce0
Esra Sümer <esrasumer28@gmail.com>
2025-02-11 01:14:54 +0300

    added image

--- my-app/src/app/components/Project/Project.jsx
+++ my-app/src/app/components/Project/Project.jsx
@@ -1,36 +1,45 @@
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from 'react';
 
-export default function Project() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+function Project() {
   return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
+    <div className='w-3/4 m-auto'>
+      <div className='mt-20'>
+        <div>
+          {data.map(d => (
+            <div key={d.name}>
+              <div className='bg-white h-[450px] text-black'>
+                <div className='h-56 bg-indigo-500 flex justify-center items-center'>
+                  <img src={d.img} alt='' className='h-44 w-44' />
+                </div>
+                <div className='flex flex-col justify-center items-center gap-4 p-4'>
+                  <p className='text-xl font-semibold'>{d.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+    </div>
   );
 }
+
+export default Project;
+
+const data = [
+  {
+    name: 'demir',
+    img: '/scrapp_image/iron_scrapp.jpg'
+  },
+  {
+    name: 'bakır',
+    img: '/scrapp_image/iron_scrapp3.jpg'
+  },
+  {
+    name: 'aliminyum',
+    img: '/scrapp_image/iron_scrapp4.jpg'
+  },
+  {
+    name: 'çelik',
+    img: '/scrapp_image/iron_scrapp5.jpg'
+  }
+];
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -5,9 +5,6 @@ import Project from './components/Project/Project'
 import Footer from './components/Footer/footer'
 import Navbar from './components/Navbar/Navbar'
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 export default function Home () {
   return (
     <div>
