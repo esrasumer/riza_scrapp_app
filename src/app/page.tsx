02d156dbb50c0a0206982e6704acd3b06ccd57ae
Esra Sümer <esrasumer28@gmail.com>
2025-02-17 01:09:33 +0300

    added whatsappicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from './components/About/About'
 import HomePage from './components/Home/HomePage'
 import Contact from './components/Contact/contact'
+import ContactWrapper from './components/Contact/CantactWrapper'
 import Footer from './components/Footer/footer'
 import Navbar from './components/Navbar/Navbar'
 import SwiperComponent from './components/Projects/SwiperComponent'
@@ -10,6 +11,7 @@ export default function Home () {
     <div>
       <Navbar />
       <HomePage />
+      <ContactWrapper />
       <About />
       <SwiperComponent />
       <Contact />
